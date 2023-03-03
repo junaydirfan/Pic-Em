40153bb6bf26c5f9088f694b7ae4dec89f8d50e0
bilawal <bilawal.imdad@safarifoneict.com>
2023-03-03 14:33:56 +0500

    Init

---
+++ Backend/src/main/java/com/picem/PicEm/PicEmApplication.java
@@ -0,0 +1,13 @@
+package com.picem.PicEm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicEmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicEmApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/picem/PicEm/PicEmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picem.PicEm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicEmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
